c40f280852c28f60b612581912b7fca004f5a685
touayouch <hassan.touayouch@ecole.ensicaen.fr>
2025-09-17 16:35:06 +0200

    complete actions in todoReducer function

--- todo-list/src/reducers/TodoReducer.js
+++ todo-list/src/reducers/TodoReducer.js
@@ -20,3 +20,83 @@ export const initialState = {
   ],
   filter: "all", // all, active, completed
 };
+
+
+export const todoReducer = (state, action) => {
+  switch (action.type) {
+    case TODO_ACTIONS.SET_TODOS:
+      return {
+        ...state,
+        todos: action.payload,
+      };
+
+    case TODO_ACTIONS.ADD:
+      return {
+        ...state,
+        todos: [...state.todos, action.payload],
+      };
+
+    case TODO_ACTIONS.UPDATE:
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload.id
+            ? { ...todo, text: action.payload.text, isEditing: false }
+            : todo
+        ),
+      };
+
+    case TODO_ACTIONS.DELETE:
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+
+    case TODO_ACTIONS.TOGGLE:
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload
+            ? { ...todo, completed: !todo.completed }
+            : todo
+        ),
+      };
+
+    case TODO_ACTIONS.START_EDIT:
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload ? { ...todo, isEditing: true } : todo
+        ),
+      };
+
+    case TODO_ACTIONS.CANCEL_EDIT:
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload ? { ...todo, isEditing: false } : todo
+        ),
+      };
+
+    case TODO_ACTIONS.SET_FILTER:
+      return {
+        ...state,
+        filter: action.payload, // "all", "active", "completed"
+      };
+
+    case TODO_ACTIONS.SET_LOADING:
+      return {
+        ...state,
+        loading: action.payload,
+      };
+
+    case TODO_ACTIONS.SET_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
